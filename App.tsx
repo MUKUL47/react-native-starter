ef974a6eec6f1324d7ae9da79eb398ac3c00adcc
mukul47 <mukul47@github.com>
2021-08-03 00:20:57 +0530

    first-commit

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+export default function App() {
+  return <View>TEST</View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    overflow: "visible",
+    maxHeight: "50%",
+  },
+});
---
+++ readme.md
@@ -0,0 +1,21 @@
+# React Native starter - with expo
+
+## Steps
+
+### Install cli
+
+```
+    npm install --global expo-cli
+```
+
+### Intialize react-native ts
+
+```
+    expo init -t expo-template-blank-typescript
+```
+
+### Install android studio
+
+```
+    sudo snap install android-studio --classic
+```
